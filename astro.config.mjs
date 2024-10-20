461265218ef64b72442bc14d147bedc0c8cd638c
Dino Lindlau <dino@lindlau.dev>
2024-10-20 15:37:56 +0200

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   site: 'https://gruppe5org.github.io', 
   base: 'archive'
 });
