6b5d3fe3f47e9d49b85669401a930522fd4c8d0a
Dino Lindlau <dino@lindlau.dev>
2024-10-24 16:05:23 +0200

    Add font, basic layout and an image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import webfontDownload from "vite-plugin-webfont-dl";
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    // @ts-ignore
+    webfontDownload(),
     react(),
     tailwind({
       applyBaseStyles: false,
